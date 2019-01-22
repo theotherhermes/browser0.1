017119940a09e1a4db4289bf9c37fb4c09ee8257
theotherhermes <rinat.luhmanov@mail.ru>
2019-01-22 11:15:20 +0300

    inital commit

---
+++ finder/Comments/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  finder
+//
+//  Created by Rinat Luhmanov on 22/01/2019.
+//  Copyright © 2019 Rinat Luhmanov. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
