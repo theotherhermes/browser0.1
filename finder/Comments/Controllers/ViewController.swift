3cc87dd7fbac0a2ad384fd6c76fd2bbb52a41314
theotherhermes <rinat.luhmanov@mail.ru>
2019-01-22 11:44:40 +0300

    add finder

--- finder/Comments/Controllers/ViewController.swift
+++ finder/Comments/Controllers/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://www.google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
